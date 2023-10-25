344aa9f5ad8ebf562016102de901ab8bcd6ea10d
Purna Shrestha <crush9918@gmail.com>
2023-10-25 19:39:59 +0545

    Change the option format and feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Question from "./components/Question";
-import Results from "./components/Results";
 import questionsData from "./json/questions.json";
 
 const App = () => {
   const [questions, setQuestions] = useState(questionsData);
   const [currentRound, setCurrentRound] = useState(1);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [showResults, setShowResults] = useState(false);
   const [userAnswer, setUserAnswer] = useState(null);
   const [score, setScore] = useState(0);
   const [showNextRoundPrompt, setShowNextRoundPrompt] = useState(false);
+  const [questionResults, setQuestionResults] = useState(Array(questionsData.length).fill(null));
 
   const maxRounds = 10;
   const questionsPerRound = 10;
@@ -21,32 +20,35 @@ const App = () => {
   );
 
   const handleAnswer = (selectedAnswer) => {
+    const currentQuestion = roundQuestions[currentQuestionIndex];
+    const isCorrect = selectedAnswer === currentQuestion.correctAnswer;
+
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const resultsCopy = [...questionResults];
+    resultsCopy[currentRound - 1 + currentQuestionIndex] = {
+      correctAnswer: currentQuestion.correctAnswer,
+      userAnswer: selectedAnswer,
+    };
+    setQuestionResults(resultsCopy);
+
     setUserAnswer(selectedAnswer);
-    checkAnswer(selectedAnswer);
-    setShowResults(true);
 
     setTimeout(() => {
       handleNextQuestion();
     }, 2000); // Automatically transition to the next question after 2 seconds
   };
 
-  const checkAnswer = (selectedAnswer) => {
-    const currentQuestion = roundQuestions[currentQuestionIndex];
-    if (selectedAnswer === currentQuestion.correctAnswer) {
-      setScore(score + 1);
-    }
-  };
-
   const handleNextQuestion = () => {
     setUserAnswer(null);
     if (currentQuestionIndex < questionsPerRound - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setShowResults(false);
     } else {
       if (currentRound < maxRounds) {
         setCurrentRound(currentRound + 1);
         setCurrentQuestionIndex(0);
-        setShowResults(false);
         setShowNextRoundPrompt(true);
       } else {
         alert("End of the quiz");
@@ -57,7 +59,6 @@ const App = () => {
   const handleNextRound = () => {
     setCurrentRound(currentRound + 1);
     setCurrentQuestionIndex(0);
-    setShowResults(false);
     setShowNextRoundPrompt(false);
   };
 
@@ -66,43 +67,58 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-4 rounded-md shadow-md relative">
-        {showResults && (
-          <Results
-            userAnswer={userAnswer}
-            correctAnswer={currentQuestion.correctAnswer}
-            score={score}
-          />
-        )}
-        {!showResults && currentQuestion && (
-          <Question
-            dialogue={currentQuestion.dialogue}
-            options={currentQuestion.options}
-            onAnswer={handleAnswer}
-          />
-        )}
-        <div className="absolute top-4 right-4">
-          <p className="text-lg font-bold">
-            {currentQuestionIndex + 1}/{questionsPerRound}
-          </p>
-          {showNextRoundPrompt ? (
-            <div>
-              <p className="text-lg">Round {currentRound - 1} is over.</p>
-              <button
-                onClick={handleNextRound}
-                className="bg-blue-500 text-white rounded-md p-2 m-1 cursor-pointer"
+        <div className="text-xl font-semibold mb-4">
+          Round {currentRound}/{maxRounds}
+        </div>
+        <div className="text-lg font-bold mb-4">
+          Question {currentQuestionIndex + 1}/{questionsPerRound}
+        </div>
+        <div className="text-lg font-semibold mb-4">
+          Score: {score}
+        </div>
+        {showNextRoundPrompt ? (
+          <div className="mb-4">
+            <p className="text-lg mb-2">Round {currentRound - 1} is over.</p>
+            <button
+              onClick={handleNextRound}
+              className="bg-blue-500 text-white rounded-md p-2 cursor-pointer"
+            >
+              Play Round {currentRound}
+            </button>
+          </div>
+        ) : (
+          <>
+            <Question
+              dialogue={currentQuestion.dialogue}
+              options={currentQuestion.options}
+            />
+            {currentQuestion.options.map((option, index) => (
+              <div
+                key={index}
+                className={`p-2 rounded-md m-1 cursor-pointer ${
+                  userAnswer === option
+                    ? option === currentQuestion.correctAnswer
+                      ? "bg-green-200 transform scale-125"
+                      : "bg-red-200"
+                    : ""
+                }`}
+                onClick={() => {
+                  if (!userAnswer) {
+                    handleAnswer(option);
+                  }
+                }}
               >
-                Play Round {currentRound}
-              </button>
-            </div>
-          ) : (
+                {option}
+              </div>
+            ))}
             <button
               onClick={handleNextQuestion}
-              className="bg-blue-500 text-white rounded-md p-2 m-1 cursor-pointer"
+              className="bg-blue-500 text-white rounded-md p-2 m-1 cursor-pointer mt-4"
             >
               Next Question
             </button>
-          )}
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
